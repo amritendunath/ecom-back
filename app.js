7ae9ef836710b625348a4a6ead7b1da6fc943d44
Amritendu Nath <amritendunath@yahoo.com>
2022-12-12 14:58:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,14 +36,7 @@ app.use('/api/v1', auth)
 app.use('/api/v1', payment)
 app.use('/api/v1', order)
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../ecom-front/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../ecom-front/build/index.html'))
-    })
-    
-}
 
 
 // Middleware to handle errors
